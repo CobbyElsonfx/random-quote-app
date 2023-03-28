77296116e1ddc9e5fcfe2ddedefe613e2da14cdf
CobbyElsonfx <andohfrancis9187@gmail.com>
2023-03-28 18:08:35 +0000

    done with the logo

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -103,7 +103,7 @@ function Quote() {
         <div>
             <div className="container mx-auto h-600  max-w-lg py-4  bg-white shadow-lg  space-y-5 rounded-lg my-20">
                 <div className="flex justify-center md:justify-end -mt-16">
-                        <img className="w-20 h-20 object-cover rounded-full border-2 border-indigo-500" src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"/>
+                        <img className="w-20 h-20 object-cover rounded-full border-2 border-indigo-500" src="./cobbyelsonfx-high-resolution-color-logo.png"/>
                 </div>
                 <div className="p-5">
                         <h2 className="text-gray-800 text-3xl font-semibold">Quotes Lab</h2>
@@ -111,7 +111,7 @@ function Quote() {
                 </div>
 
                 <div className="flex relative pt-6">
-                <a href="#" className="text-sm p-2  absolute bottom-0 left-2 font-medium text-indigo-500 text-right">{author} </a>
+                <a href="#" className="text-sm p-2  absolute bottom-0 left-2 font-medium text-indigo-500 text-right"><span>Author:</span> {author} </a>
                 <div className="absolute right-6 bottom-1"> 
                     <button  className={`shadow-lg rounded-full p-2 ${isClicked? ' text-red shadow-md shadow-red' : 'text-lightGray shadow-md shadow-lightGray'}`}  onClick={() => handleLike(quotesData)}>
                      <FaHeart size={20} />
